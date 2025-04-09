f71829a9b268f292f2413f4be72048d09ace184a
Moritz Schäfer <154064199+MosesCommitsFraud@users.noreply.github.com>
2025-04-09 21:05:45 +0200

    favicon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,11 @@ export const metadata: Metadata = {
   generator: "v0.dev",
   icons: {
     icon: [
-      { url: "public/favicon.ico" },
-      { url: "public/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "public/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
-    apple: [{ url: "public/apple-touch-icon.png" }],
+    apple: [{ url: "/apple-touch-icon.png" }],
   },
 };
 
